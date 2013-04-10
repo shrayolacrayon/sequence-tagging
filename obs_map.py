e570ee66d9b443cf5ab7cf9569af6018f1ac81e7
Mandita <ams568@cornell.edu>
2013-04-10 11:58:14 -0400

    viterbi

--- obs_map.py
+++ obs_map.py
@@ -1,3 +1,10 @@
+import nltk
+import parse
+
+(ratings, sentiments, sent_list)= parse.parse('train/ScottRenshaw_train.txt')
+
+print sentiments
+
 #returns a list of list of observations
-def sent_to_obs(sentiments):
+#def sent_to_obs(sentiments):
   
