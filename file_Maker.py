69e107438c4e42cb8bf4f58c8e020e9b923c8d05
Mandita <ams568@cornell.edu>
2013-04-10 23:00:29 -0400

    testing

--- file_Maker.py
+++ file_Maker.py
@@ -6,12 +6,15 @@ def findIndex(slist, observationList, fileName, backTrack):
 	print "This is the number of sentences: "+str(len(slist))
 	print "This is the number of observations: "+str(len(observationList))
 	print "BACKTRACK"
-	#print backTrack
+	print backTrack
 	print "The length of backtrack is : "+str(len(backTrack))
 	for t in range(0,len(slist)):
 		for o in range(0,len(observationList)):
+			print "OBBBBBBBBBBBSSSSSSSSSSSS"
+			print observationList[o]
 			if t in observationList[o]:
 				observationNumber= o
+				print "I GOTTTTT INNNNN"
 				#print "NUMBERRRRRR: "+str(observationNumber)
 				#sentenceNumber= observationList[o].index(t)
 				sentiment= backTrack[observationNumber]
