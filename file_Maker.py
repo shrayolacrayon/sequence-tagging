b38cfe82aed92a6a917518f9b367dc5636896fdc
Mandita <ams568@cornell.edu>
2013-04-11 12:22:23 -0400

    final changes

--- file_Maker.py
+++ file_Maker.py
@@ -3,28 +3,44 @@ def findIndex(slist, observationList, fileName, backTrack):
 	sentenceNumber= 0
 	output= open(fileName, "w")
 	backTrack.reverse()
-	print "This is the number of sentences: "+str(len(slist))
-	print "This is the number of observations: "+str(len(observationList))
-	print "BACKTRACK"
-	print backTrack
-	print "The length of backtrack is : "+str(len(backTrack))
+	#print "This is the number of sentences: "+str(len(slist))
+	#print "This is the number of observations: "+str(len(observationList))
+	#print "BACKTRACK"
+	#print backTrack
+	#print "The length of backtrack is : "+str(len(backTrack))
 	for t in range(0,len(slist)):
 		for o in range(0,len(observationList)):
-			#print "OBBBBBBBBBBBSSSSSSSSSSSS"
-			#print observationList[o]
 			if t in observationList[o]:
 				observationNumber= o
-				#print "I GOTTTTT INNNNN"
-				#print "NUMBERRRRRR: "+str(observationNumber)
-				#sentenceNumber= observationList[o].index(t)
 				sentiment= backTrack[observationNumber]
 				if t == 0:
-					#print "this is the first sentiment: "+str(sentiment)
 					output.write(str(sentiment))
 				else:
 					output.write("\n"+str(sentiment))
+				
 	output.close()
 				
+def findIndexSVM(slist, observationList, fileName, backTrack):
+	observationNumber= 0
+	sentenceNumber= 0
+	output= open(fileName, "w")
+	backTrack.reverse()
+	#print "This is the number of sentences: "+str(len(slist))
+	#print "This is the number of observations: "+str(len(observationList))
+	#print "BACKTRACK"
+	#print backTrack
+	#print "The length of backtrack is : "+str(len(backTrack))
+	for t in range(0,len(slist)):
+		for o in range(0,len(observationList)):
+			if t in observationList[o]:
+				observationNumber= o
+				sentiment= backTrack[observationNumber]
+				if t == 0:
+					output.write(str(sentiment))
+				else:
+					output.write("\n"+str(sentiment))
+				t= -50
+	output.close()
 
 
 
--- hmm.py
+++ hmm.py
@@ -17,7 +17,7 @@ testing = 'test_train/'
 listing = os.listdir(path)
 testlisting = os.listdir(testpath)
 #iterate through listing
-"""
+
 for fname in listing:
   ratings,sents, slist=  parse.parse(path + fname)
   #print sents
@@ -25,7 +25,7 @@ for fname in listing:
     ratingsTest, sentsTest, slistTest= parse.parse(testpath + "DennisSchwartz_test.txt")
   else:
     ratingsTest, sentsTest, slistTest= parse.parse(testpath + "ScottRenshaw_test.txt")
-  print "Length of slist at the top: "+str(len(slist))
+  #print "Length of slist at the top: "+str(len(slist))
   print "RATINGS"
   print "SENTS"
   print "P(sent)"
@@ -48,11 +48,12 @@ for fname in listing:
   print "finding the test sentences via observations..."
   testGrouped= obs_map.group_by_ngrams(ngram_dict, slist, nGramUnknown, unknownIndex, len(observations))
   #print initProbs
-  print "trying to find the sents"
+  print "trying to find the sentiments for an observation..."
   back_trace= test.virterbiTotal(aMatrix, b_matrix, testGrouped,initProbs)
   #print back_trace
-  print "creating results textFile"
+  print "creating results textFile..."
   file_Maker.findIndex(slistTest,testGrouped,testing+fname,back_trace)
+
 
 for fname in listing:
   ratings,sents, slist=  parse.parse(path + fname)
@@ -64,7 +65,7 @@ for fname in listing:
   else:
     ratingsTest, sentsTest, slistTest= parse.parse(testpath + "ScottRenshaw_test.txt")
     fileName= "ScottRenshaw_test2.txt"
-  print "Length of slist at the top: "+str(len(slist))
+  #print "Length of slist at the top: "+str(len(slist))
   print "RATINGS"
   print "SENTS"
   print "P(sent)"
@@ -90,101 +91,16 @@ for fname in listing:
   print "finding the test sentences via observations..."
   #testGrouped= obs_map.group_by_ngrams(ngram_dict, slist, nGramUnknown, unknownIndex, len(observations))
   observationsTest, obsTestIndex= parse_features.place_all_features(slistTest,ps)
-  print "I LLLLLLOOOOOOOOOVVVVVVVEEEEEE OBS"
-  #for o in range(0, len(obsTestIndex)):
-    #print obsTestIndex[o]
 
   #print initProbs
-  print "trying to find the sents"
+  print "trying to find the sentiments for an observation..."
   back_trace= test.virterbiTotal(aMatrix, b_matrix, obsTestIndex,initProbs)
   #print back_trace
-  print "creating results textFile"
+  print "creating results textFile..."
   file_Maker.findIndex(slistTest,obsTestIndex,testing+fileName,back_trace)
 
-for fname in listing:
-  ratings,sents, slist=  parse.parse(path + fname)
-  #print sents
-  if fname == "DennisSchwartz_train.txt":
-    ratingsTest, sentsTest, slistTest= parse.parse(testpath + "DennisSchwartz_test.txt")
-  else:
-    ratingsTest, sentsTest, slistTest= parse.parse(testpath + "ScottRenshaw_test.txt")
-  print "Length of slist at the top: "+str(len(slist))
-  print "RATINGS"
-  print "SENTS"
-  print "P(sent)"
-  count_s =  train.count_sentiments(sents)
-  #print count_s
-  print "S i|j"
-  aMatrix= train.sentiment_i_j(slist, count_s)
-  #print aMatrix
-  print "creating ngram..."
-  ngram_dict= train.ngrams_index(slist,1)
-  
-  print "OBSERVATIONS"
-  observations, obs_indexes, nGramUnknown, unknownIndex= obs_map.max_each_sent(ngram_dict,slist)
-  #print observations
-  #print obs_indexes
-  print "OBS PROBS"
-  b_matrix= train.observation_state(observations, slist,count_s)
-  print "Initial Probabilities"
-  initProbs= train.initialProb(sents)
-  print "finding the test sentences via observations..."
-  testGrouped= obs_map.group_by_ngrams(ngram_dict, slist, nGramUnknown, unknownIndex, len(observations))
-  #print initProbs
-  print "trying to find the sents"
-  back_trace= test.virterbiTotal(aMatrix, b_matrix, testGrouped,initProbs)
-  #print back_trace
-  print "creating results textFile"
-  file_Maker.findIndex(slistTest,testGrouped,testing+fname,back_trace)
 
-for fname in listing:
-  ratings,sents, slist=  parse.parse(path + fname)
-  fileName=""
-  #print sents
-  if fname == "DennisSchwartz_train.txt":
-    ratingsTest, sentsTest, slistTest= parse.parse(testpath + "DennisSchwartz_test.txt")
-    fileName= "DennisSchwartz_test2.txt"
-  else:
-    ratingsTest, sentsTest, slistTest= parse.parse(testpath + "ScottRenshaw_test.txt")
-    fileName= "ScottRenshaw_test2.txt"
-  print "Length of slist at the top: "+str(len(slist))
-  print "RATINGS"
-  print "SENTS"
-  print "P(sent)"
-  count_s =  train.count_sentiments(sents)
-  #print count_s
-  print "S i|j"
-  aMatrix= train.sentiment_i_j(slist, count_s)
-  #print aMatrix
-  print "creating ngram..."
-  ngram_dict= train.ngrams_index(slist,1)
-  
-  print "OBSERVATIONS"
-  u,c=polarize.train(sents)
-  ps = polarize.wordTypes(u,c)
-  observations,obsIndex = parse_features.place_all_features(slist,ps)
-  
-  #print observations
-  #print obs_indexes
-  print "OBS PROBS"
-  b_matrix= train.observation_state(observations, slist,count_s)
-  print "Initial Probabilities"
-  initProbs= train.initialProb(sents)
-  print "finding the test sentences via observations..."
-  #testGrouped= obs_map.group_by_ngrams(ngram_dict, slist, nGramUnknown, unknownIndex, len(observations))
-  observationsTest, obsTestIndex= parse_features.place_all_features(slistTest,ps)
-  print "I LLLLLLOOOOOOOOOVVVVVVVEEEEEE OBS"
-  #for o in range(0, len(obsTestIndex)):
-    #print obsTestIndex[o]
 
-  #print initProbs
-  print "trying to find the sents"
-  back_trace= test.virterbiTotal(aMatrix, b_matrix, obsTestIndex,initProbs)
-  #print back_trace
-  print "creating results textFile"
-  file_Maker.findIndex(slistTest,obsTestIndex,testing+fileName,back_trace)
-  #print train.observation_state(observations,slist,count_s)
-"""
 for fname in listing:
   ratings,sents, slist=  parse.parse(path + fname)
   fileName=""
@@ -195,7 +111,7 @@ for fname in listing:
   else:
     ratingsTest, sentsTest, slistTest= parse.parse(testpath + "ScottRenshaw_test.txt")
     fileName= "ScottRenshaw_test3.txt"
-  print "Length of slist at the top: "+str(len(slist))
+  #print "Length of slist at the top: "+str(len(slist))
   print "RATINGS"
   print "SENTS"
   print "P(sent)"
@@ -213,10 +129,6 @@ for fname in listing:
   observations,obsIndex, tout= parse_features.place_all_sentences(slist,ps,100,[])
   
 
-
-
-  #print observations
-  #print obs_indexes
   print "OBS PROBS"
   b_matrix= train.observation_state(observations, slist,count_s)
   print "Initial Probabilities"
@@ -224,14 +136,16 @@ for fname in listing:
   print "finding the test sentences via observations..."
   #testGrouped= obs_map.group_by_ngrams(ngram_dict, slist, nGramUnknown, unknownIndex, len(observations))
   observationsTest, obsTestIndex, toutTest= parse_features.place_all_sentences(slistTest,ps,100, tout)
-  print "I LLLLLLOOOOOOOOOVVVVVVVEEEEEE OBS"
-  #for o in range(0, len(obsTestIndex)):
-    #print obsTestIndex[o]
 
   #print initProbs
-  print "trying to find the sents"
+  print "trying to find the sentiments for observations..."
   back_trace= test.virterbiTotal(aMatrix, b_matrix, obsTestIndex,initProbs)
   #print back_trace
-  print "creating results textFile"
-  file_Maker.findIndex(slistTest,obsTestIndex,testing+fileName,back_trace)
+  print "creating results textFile..."
+  if "Dennis" in fname:
+    file_Maker.findIndex(slistTest,obsTestIndex,testing+fileName,back_trace)
+  else:
+    file_Maker.findIndexSVM(slistTest,obsTestIndex,testing+fileName,back_trace)
+
+
   
